15db27ed2864302bcf7c05ec9fe25a88104fd568
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-07-03 16:57:09 -0500

    timer

--- unity-assets_models_textures/Assets/Scripts/PlayerController.cs
+++ unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -5,32 +5,77 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody Player;
-    public float speed = 5.0f;
+    private float speed = 8.0f;
+    private float jumpVelocity = 8.0f;
+    private float fallVelocity = 5.5f;
+    private float lowJumpMultiplier = 9f;
+    bool isGrounded;
+    public Camera playerCamera;
+
     void Start()
     {
         Player = GetComponent<Rigidbody>();
     }
 
-    void FixedUpdate()
+    void Update()
     {
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
-            Player.transform.position += Player.transform.forward * speed * Time.deltaTime;
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            Player.transform.position -= Player.transform.right * speed * Time.deltaTime;
+            Player.MovePosition(Player.position + playerCamera.transform.forward * speed * Time.deltaTime);
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            Player.transform.position -= Player.transform.forward * speed * Time.deltaTime;
+            Player.MovePosition(Player.position - playerCamera.transform.forward * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Player.MovePosition(Player.position - playerCamera.transform.right * speed * Time.deltaTime);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            Player.transform.position += Player.transform.right * speed * Time.deltaTime;
+            Player.MovePosition(Player.position + playerCamera.transform.right * speed * Time.deltaTime);
+        }
+
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            Player.velocity = Vector3.up * jumpVelocity;
+        }
+
+        if (Player.velocity.y < 0)
+        {
+            Player.velocity += Vector3.up * Physics.gravity.y * (fallVelocity - 1) * Time.deltaTime;
+        }
+
+        else if (Player.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+        {
+            Player.velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+
+        if (collision.gameObject.CompareTag("Kill Zone"))
+        {
+            Player.transform.position = new Vector3(0, 15.25f, 0);
+            Player.transform.rotation = new Quaternion(0, 0, 0, 0);
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
         }
     }
 }
