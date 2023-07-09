a810786e90068970d9127fd81a3a394e6521299f
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-07-09 07:25:06 -0500

    task 1

---
+++ unity-assets_ui/Assets/Scripts/WinTrigger.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinTrigger : MonoBehaviour
+{
+    public Text Timer;
+    private Timer Script;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject player = GameObject.Find("Player");
+        Script = player.GetComponent<Timer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Script.StopTimer();
+            Script.timerText.color = Color.green;
+            Script.timerText.rectTransform.localScale = new Vector3(.5f, .5f, .5f);
+        }
+
+    }
+}
